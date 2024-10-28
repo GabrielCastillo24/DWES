c3fd361a23acc9b97c8a36f453835757a0168bee
Gabriel Castillo <gcastillol24@fpcoruna.afundacion.org>
2024-10-28 09:59:05 +0100

    Añadido detail.php y comment.php para que se guarden los nuevos comentarios en nuestra base de datos y se reflejen en detail.php

---
+++ sprint2phpsql/www/mysite/comment.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comment</title>
+</head>
+<body>
+    <?php
+    $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+    ?>
+
+<?php
+
+    // Obtener valores del formulario
+    $libro_id = $_POST['libroId'];
+    $comentario = $_POST['new_comment'];
+    
+    // Definir y ejecutar la consulta
+    $query = "INSERT INTO tCOMENTARIOS(comentario, libroId, usuarioId)
+              VALUES ('".$comentario."', ".$libro_id.", NULL)";
+    mysqli_query($db, $query) or die('Error en la inserción');
+
+    // Confirmación de inserción
+    echo "<p>Nuevo comentario ";
+    echo mysqli_insert_id($db);
+    echo " añadido</p>";
+
+    echo "<a href='/detail.php?libroid=".$libro_id."'>Volver</a>";
+
+    // Cerrar la conexión
+    mysqli_close($db);
+?>
+</body>
+</html>
--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -13,32 +13,40 @@
 </head>
 <body>
 <!--Coneccion con la base de datos-->
-<?php
-$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
-?>
+    <?php
+    $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+    ?>
 
-<?php
-if (!isset($_GET['id'])) {
-die('No se ha especificado una canción');
-}
-$libro_id = $_GET['id'];
-$query = 'SELECT * FROM tLIBROS WHERE id='.$libro_id;
-$result = mysqli_query($db, $query) or die('Query error');
-$only_row = mysqli_fetch_array($result);
-echo '<h1>'.$only_row['nombre'].'</h1>';
-echo '<h2>'.$only_row['autor'].'</h2>';
-echo  "<img src='".$only_row['url_imagen']."'/>";
-?>
+    <?php
+        if (!isset($_GET['libroid'])) {
+        die('No se ha especificado un libro');
+        }
+        $libro_id = $_GET['libroid'];
+        $query = 'SELECT * FROM tLIBROS WHERE id='.$libro_id;
+        $result = mysqli_query($db, $query) or die('Query error');
+        $only_row = mysqli_fetch_array($result);
+        echo '<h1>'.$only_row['nombre'].'</h1>';
+        echo '<h2>'.$only_row['autor'].'</h2>';
+        echo  "<img src='".$only_row['url_imagen']."'/>";
+    ?>
 <h3>Comentarios:</h3>
 <ul>
-<?php
-$query2 = 'SELECT * FROM tCOMENTARIOS WHERE libroId='.$libro_id;
-$result2 = mysqli_query($db, $query2) or die('Query error');
-while ($row = mysqli_fetch_array($result2)) {
-echo '<li>'.$row['comentario'].'</li>';
-}
-mysqli_close($db);
-?>
+    <?php
+        $query2 = 'SELECT * FROM tCOMENTARIOS WHERE libroId='.$libro_id;
+        $result2 = mysqli_query($db, $query2) or die('Query error');
+        while ($row = mysqli_fetch_array($result2)) {
+        echo '<li>'.$row['comentario'].'</li>';
+        }
+        mysqli_close($db);
+    ?>
+
+    <p>Deja un nuevo comentario:</p>
+    <form action="/comment.php" method="post">
+        <textarea rows="4" cols="50" name="new_comment"></textarea><br>
+        <input type="hidden" name="libroId" value="<?php echo $libro_id;?>">
+        <input type="submit" value="Comentar">
+    </form>
+
 </ul>
 </body>
 </html>
