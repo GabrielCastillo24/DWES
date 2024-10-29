da4d162e228d00c89503d69b694d50dfe80f2e5d
Gabriel Castillo <gcastillol24@fpcoruna.afundacion.org>
2024-10-29 12:51:53 +0100

    Creado structura nueva de carpetas para el nuevo sprint3sesiones

---
+++ sprint3sesiones/www/mysite/detail.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail</title>
+    <style>
+        img{
+            width: 100px;
+            height: 100px;
+        }
+    </style>
+</head>
+<body>
+<!--Coneccion con la base de datos-->
+    <?php
+    $db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+    ?>
+
+    <?php
+        $fecha = date("Y-m-d H:i:s"); 
+        echo $fecha;
+        if (!isset($_GET['libroid'])) {
+        die('No se ha especificado un libro');
+        }
+        $libro_id = $_GET['libroid'];
+        $query = 'SELECT * FROM tLIBROS WHERE id='.$libro_id;
+        $result = mysqli_query($db, $query) or die('Query error');
+        $only_row = mysqli_fetch_array($result);
+        echo '<h1>'.$only_row['nombre'].'</h1>';
+        echo '<h2>'.$only_row['autor'].'</h2>';
+        echo  "<img src='".$only_row['url_imagen']."'/>";
+    ?>
+<h3>Comentarios:</h3>
+<ul>
+    <?php
+        $query2 = 'SELECT * FROM tCOMENTARIOS WHERE libroId='.$libro_id;
+        $result2 = mysqli_query($db, $query2) or die('Query error');
+        while ($row = mysqli_fetch_array($result2)) {
+        echo '<li>'.$row['comentario'].$row['fechaComentario'].'</li>';
+        }
+        mysqli_close($db);
+    ?>
+
+    <p>Deja un nuevo comentario:</p>
+    <form action="/comment.php" method="post">
+        <textarea rows="4" cols="50" name="new_comment"></textarea><br>
+        <input type="hidden" name="libroId" value="<?php echo $libro_id;?>">
+        <input type="hidden" name="fechaComentario" value="<?php echo $fecha;?>">
+        <input type="submit" value="Comentar">
+    </form>
+
+</ul>
+</body>
+</html>
---
+++ sprint3sesiones/www/mysite/main.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicios</title>
+    <style>
+        img{
+            width: 100px;
+            height: 100px;
+        }
+    </style>
+</head>
+<body>
+<!--ESTABLECE CONECCION A LA BASE DE DATOS-->
+<?php
+$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+?>
+<h1>Conexión establecida</h1>
+<?php 
+   
+   $query = 'SELECT * FROM tLIBROS';
+   $result = mysqli_query($db, $query);
+
+   // Verificar si la consulta fue exitosa
+   if ($result) {
+       // Recorrer los resultados y mostrarlos
+       echo "<ul>";
+       while ($row = mysqli_fetch_array($result)) {
+           // Imprimir el valor del campo 'nombre' de cada fila
+         
+           echo "<li>id: " . $row['id'] . "</li>";
+           echo "<li>Nombre: " . $row['nombre'] . "</li>";
+           echo "<li><img src='".$row['url_imagen']."'/></li>";
+           echo "<li>Autor: " . $row['autor'] . "</li>";
+           echo "<li>numero Paginas: " . $row['numPaginas'] . "</li>";
+           echo "<br>";
+
+       }
+       echo "</ul>";
+   } else {
+       // Mostrar mensaje si la consulta falló
+       echo "<p>Error en la consulta: " . mysqli_error($db) . "</p>";
+   }
+   mysqli_close($db);
+ ?> 
+</body>
+</html>
