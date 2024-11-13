eef6b1f5dc8fcdb569b5903ce0ca0983627a401f
Gabriel Castillo <gcastillol24@fpcoruna.afundacion.org>
2024-11-13 13:17:10 +0100

    Tarea 1 de python creado archivo operaciones.py

---
+++ sprint3python/operaciones.py
@@ -0,0 +1,27 @@
+class Operaciones:
+    valor1 = valor1
+    valor2 = valor2
+
+    def __init__(self,valor1,valor2):
+        self.valor1 = valor1
+        self.valor2 = valor2
+
+    def sumar(valor1,valor2):
+        return valor1 + valor2
+
+    def restar(valor1,valor2):
+        return valor1 + valor2
+
+    def multiplicar(valor1, valor2):
+        return valor2 * valor2
+
+    def dividir(valor1, valor2):
+        if valor1 == 0 :
+            print("no se puede dividir con 0")
+        elif valor2 == 0:
+            print("no se puede dividir con 0")
+        else:
+            valor1/valor2
+
+
+
