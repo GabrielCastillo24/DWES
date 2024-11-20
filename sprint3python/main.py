6164b40e0ff96e5d1d756bf73d2ff840a10bc860
Gabriel Castillo <gcastillol24@fpcoruna.afundacion.org>
2024-11-20 14:40:38 +0100

    Hemos creado nuestro entorno Django y emos instanceado las tablas de nuestra base de datos mysitedb a clases python en nuestro proyecto

--- sprint3python/main.py
+++ sprint3python/main.py
@@ -2,13 +2,15 @@ from heroe import Heroe
 from mazmorra import Mazmorra
 
 def main():
-
+    #Pedimos el nombre del heroe por consola 
     nombreHeroe = input("Introduce un nombre de heroe : ")
-    
+    #Creamos el heroe 
     heroe = Heroe(nombreHeroe)
     
+    #Creamos la mazmorra con el heroe 
     mazmorra = Mazmorra(heroe)
 
+    #Lamada al metodo jugar()
     mazmorra.jugar()
     
 
