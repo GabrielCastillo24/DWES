a959f0fdede77370fe35670aa61e045c54e317ac
Gabriel Castillo <gcastillol24@fpcoruna.afundacion.org>
2024-11-21 11:42:55 +0100

    Primer endpoint: Devolver un json con todos los libros

--- sprint3django/RestAPI/RestAPI/urls.py
+++ sprint3django/RestAPI/RestAPI/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webserviceapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test',views.pagina_de_prueba),
+    path('canciones',views.devolver_canciones)
 ]
--- sprint3django/RestAPI/webserviceapp/views.py
+++ sprint3django/RestAPI/webserviceapp/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from .models import Tlibros
 
 # Create your views here.
+def pagina_de_prueba(request):
+        return HttpResponse("<h1> Hola caracola </h1>")
 
+def devolver_canciones(request):
+        lista = Tlibros.objects.all()
+        respuesta_final = []
+        for fila_sql in lista:
+                diccionario = {}
+                diccionario['id'] =fila_sql.id
+                diccionario['nombre'] = fila_sql.nombre
+                diccionario['url_img']= fila_sql.url_imagen
+                diccionario['autor'] = fila_sql.autor
+                diccionario['numpaginas'] = fila_sql.numpaginas
+                respuesta_final.append(diccionario)
+        return JsonResponse(respuesta_final, safe=False)
